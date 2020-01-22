c59e8b0240e6a84152a27e970fefb02cff2e5d1c
yukimori <yuharacomplex@gmail.com>
2020-01-22 13:11:24 +0900

    コメントの追加

--- Chapter1/unbreakable_encryption.py
+++ Chapter1/unbreakable_encryption.py
@@ -19,15 +19,20 @@ from typing import Tuple
 
 def random_key(length: int) -> int:
     # generate length random bytes
+    # length長のランダムバイト作成
     tb: bytes = token_bytes(length)
     # convert those bytes into a bit string and return it
+    # バイトをビット列にして返す
+    # from_bytes: 7バイトをとって56ビット整数に変換する
     return int.from_bytes(tb, "big")
 
 
 def encrypt(original: str) -> Tuple[int, int]:
+    # 文字列をUTF-8バイトシーケンスに変換する
     original_bytes: bytes = original.encode()
     dummy: int = random_key(len(original_bytes))
     original_key: int = int.from_bytes(original_bytes, "big")
+    # XOR演算
     encrypted: int = original_key ^ dummy  # XOR
     return dummy, encrypted
 
@@ -35,6 +40,7 @@ def encrypt(original: str) -> Tuple[int, int]:
 def decrypt(key1: int, key2: int) -> str:
     decrypted: int = key1 ^ key2  # XOR
     temp: bytes = decrypted.to_bytes((decrypted.bit_length() + 7) // 8, "big")
+    # バイトシーケンスを文字列に戻している
     return temp.decode()
 
 
